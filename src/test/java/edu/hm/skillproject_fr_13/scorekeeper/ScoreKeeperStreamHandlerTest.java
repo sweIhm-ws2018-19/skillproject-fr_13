19ff8068f3b94235fcbd2322467a253cc58940bd
sam <samuel_schill@gmx.net>
2018-11-29 16:34:14 +0100

    changed package name

---
+++ src/test/java/edu/hm/skillproject_fr_13/scorekeeper/ScoreKeeperStreamHandlerTest.java
@@ -0,0 +1,16 @@
+package edu.hm.skillproject_fr_13.scorekeeper;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ScoreKeeperStreamHandlerTest {
+
+	@DisplayName ("This class will not be tested for now -Anton 2018-11-26")
+	@Test
+	public void testEnabled() {
+		assertTrue(true);
+	}
+	
+}
